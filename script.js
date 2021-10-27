af6c7566f8592bb0f9bbdcdfb5a6c13bedf45f49
alizhassane <91382332+alizhassane@users.noreply.github.com>
2021-10-27 20:49:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+  function expérience() {
+    document.getElementById("cadre").src="Expérience.html";
+  }
+  function compétences() {
+    document.getElementById("cadre").src="Competence.html";
+  }
+  function formation() {
+    document.getElementById("cadre").src="Formation.html";
+  }
+  /*fait lovertur de les 3 page dans la partie section */
